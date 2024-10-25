e6f8b5f056a470bd452d4f61d82d5993e87fae7b
Ella <4710635+ellatrix@users.noreply.github.com>
2024-10-25 14:30:49 +0200

    Fix notic

--- lib/compat/wordpress-6.8/preload.php
+++ lib/compat/wordpress-6.8/preload.php
@@ -10,13 +10,15 @@
  */
 function gutenberg_block_editor_preload_paths_6_8( $paths, $context ) {
 	if ( 'core/edit-site' === $context->name || 'core/edit-post' === $context->name ) {
-		$paths[] = '/wp/v2/global-styles/themes/' . get_stylesheet() . '?context=view';
+		$stylesheet = get_stylesheet();
+		$global_styles_path = '/wp/v2/global-styles/themes/' . $stylesheet;
+		$paths[] = $global_styles_path . '?context=view';
+		foreach ($paths as $key => $path) {
+			if ($path === $global_styles_path) {
+				unset($paths[$key]);
+			}
+		}
 	}
-	return array_diff(
-		$paths,
-		array(
-			'/wp/v2/global-styles/themes/' . get_stylesheet(),
-		)
-	);
+	return $paths;
 }
 add_filter( 'block_editor_rest_api_preload_paths', 'gutenberg_block_editor_preload_paths_6_8', 10, 2 );
