a04bb11e1cfc625fd97a78bf0fc87434f213b767
ramon <ramonjd@gmail.com>
2024-10-25 17:24:04 +1100

    Cleaning up, trying to make things logical and abstracted.

--- lib/global-styles-and-settings.php
+++ lib/global-styles-and-settings.php
@@ -27,12 +27,10 @@ function gutenberg_get_global_stylesheet( $types = array() ) {
 			return $cached;
 		}
 	}
-	$tree = WP_Theme_JSON_Resolver_Gutenberg::get_merged_data();
-	$tree = WP_Theme_JSON_Resolver_Gutenberg::resolve_theme_file_uris( $tree );
-
-	var_dump( WP_Theme_JSON_Resolver_Gutenberg::get_migrated_relative_theme_uris( $tree ) );
-
+	$tree                = WP_Theme_JSON_Resolver_Gutenberg::get_merged_data();
+	$tree                = WP_Theme_JSON_Resolver_Gutenberg::resolve_theme_file_uris( $tree );
 	$supports_theme_json = wp_theme_has_theme_json();
+
 	if ( empty( $types ) && ! $supports_theme_json ) {
 		$types = array( 'variables', 'presets', 'base-layout-styles' );
 	} elseif ( empty( $types ) ) {
