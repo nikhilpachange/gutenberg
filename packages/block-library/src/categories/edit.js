53bb5ff55bad2e9e874b7588faa089a6d0fe8cb9
Mario Santos <santosguillamot@gmail.com>
2024-10-28 10:32:46 +0100

    Adapt categories edit file

--- packages/block-library/src/categories/edit.js
+++ packages/block-library/src/categories/edit.js
@@ -16,16 +16,69 @@ import {
 } from '@wordpress/components';
 import { useInstanceId } from '@wordpress/compose';
 import {
+	BlockContextProvider,
+	__experimentalUseBlockPreview as useBlockPreview,
 	InspectorControls,
 	useBlockProps,
+	useInnerBlocksProps,
+	store as blockEditorStore,
 	RichText,
 } from '@wordpress/block-editor';
 import { decodeEntities } from '@wordpress/html-entities';
 import { __, sprintf } from '@wordpress/i18n';
 import { pin } from '@wordpress/icons';
 import { useEntityRecords } from '@wordpress/core-data';
+import { memo, useMemo, useState } from '@wordpress/element';
+import { useSelect } from '@wordpress/data';
+
+const TEMPLATE = [ [ 'core/term-title' ] ];
+
+function CategoriesItemBlock() {
+	const innerBlocksProps = useInnerBlocksProps(
+		{ className: clsx( 'wp-block-taxonomy' ) },
+		{ template: TEMPLATE }
+	);
+	return <li { ...innerBlocksProps } />;
+}
+
+function CategoriesItemBlockPreview( {
+	blocks,
+	blockContextId,
+	setActiveBlockContextId,
+	isHidden,
+} ) {
+	const blockPreviewProps = useBlockPreview( {
+		blocks,
+		props: {
+			className: clsx( 'wp-block-taxonomy' ),
+		},
+	} );
+
+	const handleOnClick = () => {
+		setActiveBlockContextId( blockContextId );
+	};
+
+	const style = {
+		display: isHidden ? 'none' : undefined,
+	};
+
+	return (
+		<li
+			{ ...blockPreviewProps }
+			tabIndex={ 0 }
+			// eslint-disable-next-line jsx-a11y/no-noninteractive-element-to-interactive-role
+			role="button"
+			onClick={ handleOnClick }
+			onKeyPress={ handleOnClick }
+			style={ style }
+		/>
+	);
+}
+
+const MemoizedCategoriesItemBlockPreview = memo( CategoriesItemBlockPreview );
 
 export default function CategoriesEdit( {
+	clientId,
 	attributes: {
 		displayAsDropdown,
 		showHierarchy,
@@ -66,10 +119,28 @@ export default function CategoriesEdit( {
 		query.parent = 0;
 	}
 
+	const [ activeBlockContextId, setActiveBlockContextId ] = useState();
 	const { records: categories, isResolving } = useEntityRecords(
 		'taxonomy',
 		taxonomySlug,
 		query
+	);
+
+	const blocks = useSelect(
+		( select ) => {
+			const { getBlocks } = select( blockEditorStore );
+			return getBlocks( clientId );
+		},
+		[ clientId ]
+	);
+
+	const blockContexts = useMemo(
+		() =>
+			categories?.map( ( term ) => ( {
+				taxonomyType: term.taxonomy,
+				taxonomyId: term.id,
+			} ) ),
+		[ categories ]
 	);
 
 	const getCategoriesList = ( parentId ) => {
@@ -89,34 +160,58 @@ export default function CategoriesEdit( {
 		! name ? __( '(Untitled)' ) : decodeEntities( name ).trim();
 
 	const renderCategoryList = () => {
-		const parentId = isHierarchicalTaxonomy && showHierarchy ? 0 : null;
-		const categoriesList = getCategoriesList( parentId );
-		return categoriesList.map( ( category ) =>
-			renderCategoryListItem( category )
+		return (
+			<>
+				{ blockContexts &&
+					blockContexts.map( ( blockContext ) => (
+						<BlockContextProvider
+							key={ blockContext.taxonomyId }
+							value={ blockContext }
+						>
+							{ blockContext.taxonomyId ===
+							( activeBlockContextId ||
+								blockContexts[ 0 ]?.taxonomyId ) ? (
+								<CategoriesItemBlock />
+							) : null }
+							<MemoizedCategoriesItemBlockPreview
+								blocks={ blocks }
+								blockContextId={ blockContext.taxonomyId }
+								setActiveBlockContextId={
+									setActiveBlockContextId
+								}
+								isHidden={
+									blockContext.taxonomyId ===
+									( activeBlockContextId ||
+										blockContexts[ 0 ]?.taxonomyId )
+								}
+							/>
+						</BlockContextProvider>
+					) ) }
+			</>
 		);
 	};
 
-	const renderCategoryListItem = ( category ) => {
-		const childCategories = getCategoriesList( category.id );
-		const { id, link, count, name } = category;
-		return (
-			<li key={ id } className={ `cat-item cat-item-${ id }` }>
-				<a href={ link } target="_blank" rel="noreferrer noopener">
-					{ renderCategoryName( name ) }
-				</a>
-				{ showPostCounts && ` (${ count })` }
-				{ isHierarchicalTaxonomy &&
-					showHierarchy &&
-					!! childCategories.length && (
-						<ul className="children">
-							{ childCategories.map( ( childCategory ) =>
-								renderCategoryListItem( childCategory )
-							) }
-						</ul>
-					) }
-			</li>
-		);
-	};
+	// const renderCategoryListItem = ( category ) => {
+	// 	const childCategories = getCategoriesList( category.id );
+	// 	const { id, link, count, name } = category;
+	// 	return (
+	// 		<li key={ id } className={ `cat-item cat-item-${ id }` }>
+	// 			<a href={ link } target="_blank" rel="noreferrer noopener">
+	// 				{ renderCategoryName( name ) }
+	// 			</a>
+	// 			{ showPostCounts && ` (${ count })` }
+	// 			{ isHierarchicalTaxonomy &&
+	// 				showHierarchy &&
+	// 				!! childCategories.length && (
+	// 					<ul className="children">
+	// 						{ childCategories.map( ( childCategory ) =>
+	// 							renderCategoryListItem( childCategory )
+	// 						) }
+	// 					</ul>
+	// 				) }
+	// 		</li>
+	// 	);
+	// };
 
 	const renderCategoryDropdown = () => {
 		const parentId = isHierarchicalTaxonomy && showHierarchy ? 0 : null;
