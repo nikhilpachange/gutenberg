97265492bea41f851540a2ab308f1e4bded9b136
Dave Smith <getdavemail@gmail.com>
2024-11-08 12:01:25 +0000

    Fix linting

--- lib/compat/wordpress-6.8/class-gutenberg-rest-post-archive-search-handler.php
+++ lib/compat/wordpress-6.8/class-gutenberg-rest-post-archive-search-handler.php
@@ -114,14 +114,12 @@ class Gutenberg_REST_Post_Archive_Search_Handler extends WP_REST_Search_Handler 
 	 *
 	 * @since 6.8.0
 	 *
-	 * @param int $id Item ID.
 	 * @return array[] Array of link arrays for the given item.
 	 */
-	public function prepare_item_links( $id ) {
+	public function prepare_item_links() {
 
 		$links = array();
 
 		return $links;
 	}
 }
-
