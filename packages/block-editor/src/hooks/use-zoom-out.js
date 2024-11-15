040ebafc65347ea7d61c547969adf4a3ee76f4cb
Jerry Jones <jones.jeremydavid@gmail.com>
2024-11-15 10:21:49 -0600

    Add controlled concept to useZoomOut hook

--- packages/block-editor/src/hooks/use-zoom-out.js
+++ packages/block-editor/src/hooks/use-zoom-out.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { useSelect, useDispatch } from '@wordpress/data';
-import { useEffect } from '@wordpress/element';
+import { useEffect, useRef } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -21,16 +21,43 @@ export function useZoomOut( enabled = true ) {
 	);
 	const { isZoomOut } = unlock( useSelect( blockEditorStore ) );
 
+	const isZoomedOut = isZoomOut();
+	// If starting from zoom out engaged, do not control zoom level for the user.
+	const controlZoomLevel = useRef( ! isZoomedOut );
+
 	useEffect( () => {
-		if ( ! enabled ) {
+		if ( ! enabled || ! controlZoomLevel.current ) {
 			return;
 		}
-
 		const isAlreadyInZoomOut = isZoomOut();
 		if ( ! isAlreadyInZoomOut ) {
 			setZoomLevel( 'auto-scaled' );
 		}
 
-		return () => ! isAlreadyInZoomOut && resetZoomLevel();
+		return () => {
+			return (
+				controlZoomLevel.current &&
+				! isAlreadyInZoomOut &&
+				resetZoomLevel()
+			);
+		};
 	}, [ enabled, isZoomOut, resetZoomLevel, setZoomLevel ] );
+
+	/**
+	 * This hook tracks if the zoom state was changed manually by the user via clicking
+	 * the zoom out button.
+	 */
+	useEffect( () => {
+		// If the zoom state changed (isZoomOut) and it does not match the requested zoom
+		// state (zoomOut), then it means the user manually changed the zoom state while
+		// this hook was mounted, and we should no longer control the zoom state.
+		if ( isZoomedOut !== enabled ) {
+			// Turn off all automatic zooming control.
+			controlZoomLevel.current = false;
+		}
+
+		// Intentionally excluding `enabled` from the dependency array. We want to catch instances where
+		// the zoom out state changes due to user interaction and not due to the hook.
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [ isZoomedOut ] );
 }
